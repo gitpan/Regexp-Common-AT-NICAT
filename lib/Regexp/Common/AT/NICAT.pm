db348075e936f6881935524c40fa95b774e51602
Marcel Grünauer <marcel@cpan.org>
2010-03-27 14:27:59 +0000

    Import of MARCEL/Regexp-Common-AT-NICAT-1.100860 from CPAN.
    gitpan-cpan-distribution: Regexp-Common-AT-NICAT
    gitpan-cpan-version:      1.100860
    gitpan-cpan-path:         MARCEL/Regexp-Common-AT-NICAT-1.100860.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Regexp/Common/AT/NICAT.pm
+++ lib/Regexp/Common/AT/NICAT.pm
@@ -1,35 +1,49 @@
-package Regexp::Common::AT::NICAT;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Regexp::Common::AT::NICAT;
+our $VERSION = '1.100860';
+# ABSTRACT: Defines patterns for nic.at person handles
 use Regexp::Common 'pattern';
-
-
-our $VERSION = '0.01';
-
-
 pattern
-    name   => [ 'at', 'nicathandle' ],
-    create => '[A-Z]{2,4}\d+-NICAT';
-
-
+  name   => [qw(at nicathandle)],
+  create => '[A-Z]{2,4}\d+-NICAT';
 1;
 
 
 __END__
+=pod
+
+=for stopwords nic.at
 
 =head1 NAME
 
-Regexp::Common::AT::NICAT - Defines patterns for NICAT objects
+Regexp::Common::AT::NICAT - Defines patterns for nic.at person handles
+
+=head1 VERSION
+
+version 1.100860
 
 =head1 SYNOPSIS
 
-None.
+    use Regexp::Common 'AT::NICAT';
+
+    my $value = '...';
+    if ($value =~ /$RE{at}{nicathandle}/) {
+       #  ...
+    }
 
 =head1 DESCRIPTION
 
-This module defines patterns for NICAT (Austrian domain registry) objects to
-be used with L<Regexp::Common>.
+This module defines patterns for nic.at (Austrian domain registry) person
+handles to be used with L<Regexp::Common>.
+
+Please consult the manual of L<Regexp::Common> for a general description of
+the works of this interface. Do not use this module directly, but load it via
+I<Regexp::Common>.
+
+=head1 PATTERNS
 
 At the moment there is only one pattern, but more patterns for .at domain
 names will follow - they have some restrictions on length and characters to be
@@ -39,43 +53,43 @@ used.
 
 =item $RE{at}{nicathandle}
 
-Provides a regex to match NICAT person handles.
+Provides a regex to match nic.at person handles.
 
 =back
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<regexpcommonatnicat> tag.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-regexp-common-at-nicat@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
 
 =head1 INSTALLATION
 
 See perlmodinstall for information and options on installing Perl modules.
 
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Regexp-Common-AT-NICAT>.
+
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Regexp-Common-AT-NICAT/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Regexp-Common-AT-NICAT/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
